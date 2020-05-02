f76fba90a3efece9a7d3ba1b4cfa8f076f7d4efe
Gary Thomas <gary.thomas@piksel.com>
2020-05-02 19:11:29 +0100

    Remove ServiceConfig

--- libbaseservice/servicefactory.cpp
+++ libbaseservice/servicefactory.cpp
@@ -1,7 +1,5 @@
 #include "serviceconfig.h"
 #include "servicefactory.h"
-#include "versionresource.h"
-#include "studentofferresource.h"
 
 using namespace std;
 using namespace restbed;
@@ -9,15 +7,13 @@ using namespace restbed;
 namespace BaseService
 {
 
-    unique_ptr<JsonService> ServiceFactory::MakeService(shared_ptr<ServiceConfig> serviceConfig, uint16_t port)
+    unique_ptr<JsonService> ServiceFactory::MakeService(uint16_t port)
     {
         auto settings = make_shared< Settings >( );
         settings->set_port( port );
         settings->set_default_header( "Connection", "close" );
 
         auto service = make_unique<JsonService>(settings);
-        service->Publish(make_shared<VersionResource>(serviceConfig));
-        service->Publish(make_shared<StudentOfferResource>(serviceConfig));
         return service;
     }
 
